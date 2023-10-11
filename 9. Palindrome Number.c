60b7f56e92cdda709aa8d5607eab75434aaef700
Pratham R Shetty <96129332+praths71018@users.noreply.github.com>
2023-10-11 05:49:08 +0530

    Update 9. Palindrome Number.c

--- 9. Palindrome Number.c
+++ 9. Palindrome Number.c
@@ -1,3 +1,18 @@
+// Optimal Solution found
+bool isPalindrome(int x){
+   long rem=0,sum=0,num=x;
+   while(x>0)
+   {
+        rem=x%10; //1%10
+        sum=sum*10+rem; //10*1
+        x/=10; //10/1
+   }
+    if(sum==num)
+        return true;
+    else
+        return false;
+}
+
 // My Solution (Constraint: Can't convert integer to string)
 bool isPalindrome(int x){
     if(x<0)
