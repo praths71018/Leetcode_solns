c8023e12ee37f611e75d7a9bd0069aee521efc33
Pratham R Shetty <96129332+praths71018@users.noreply.github.com>
2024-10-30 06:12:31 +0530

    [463] [Time Beats: 5.11%] [Memory Beats: 99.90%] - LeetPush

---
+++ c++/math/463-Island-Perimeter.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();j++){
+                if(grid[i][j]==1){
+                    perimeter+=4;
+                    if(i>0 && grid[i-1][j]==1){
+                        perimeter-=1;
+                    }
+                    if(i+1<grid.size() && grid[i+1][j]==1){
+                        perimeter-=1;
+                    }
+                    if(j>0 && grid[i][j-1]==1){
+                        perimeter-=1;
+                    }
+                    if(j+1<grid[i].size() && grid[i][j+1]==1){
+                        perimeter-=1;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+};
